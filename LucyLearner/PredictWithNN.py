6e59184c5bd56190ea2ba0e5a296ca9391abfc3f
Eric Rudisaile <eric.rudisaile@gmail.com>
2017-01-19 07:59:19 -0500

    keras NN succcessfully trained, and used to predict.  not integrated with c# code yet

--- LucyLearner/PredictWithNN.py
+++ LucyLearner/PredictWithNN.py
@@ -1,4 +1,5 @@
-from keras.models import Sequential
+from keras.models import Sequential, model_from_json
+import numpy as np
 
 def LoadSavedModel():
     # load json and create model
@@ -9,6 +10,15 @@ def LoadSavedModel():
     # load weights into new model
     loaded_model.load_weights("model/model.h5")
     print("Loaded model from disk")
+    return loaded_model
 
-def DrawCardChoice(state):
+def DrawCardChoice():
     model = LoadSavedModel()
+    model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+    situation = np.array([[1,0,0,0,0,0,0,0,0,0,0,0,0]])
+    print model.predict(situation)
+
+DrawCardChoice()
+
